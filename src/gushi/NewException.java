6fc3b0fc395e6eb26fe2f6a5a84b1c707ac909f5
yusu-chen <57743900+yusu-chen@users.noreply.github.com>
2019-11-19 13:31:07 +0800

    Add files via upload

---
+++ src/gushi/NewException.java
@@ -0,0 +1,10 @@
+package gushi;
+
+public class NewException extends Exception{
+	public NewException(){
+		
+ 	}
+	public NewException(String str){ 
+            super(str);
+ 	}
+}
---
+++ src/gushi/Test.java
@@ -0,0 +1,32 @@
+package gushi;
+
+import gushi.*;
+public class Test {
+	public static void main(String args[]) throws NewException
+	{
+		Changhenge changhenge;
+
+		String write;
+		write = "千";
+		try {
+		changhenge = new Changhenge(write);
+		if(write == "") 
+		{
+			throw new NewException("不能输入空字符");
+		}
+		else 
+		
+			System.out.print(changhenge);
+		}
+		catch (NewException e) 
+		{
+			e.printStackTrace();
+		}
+
+		finally 
+		{
+			System.out.print("程序运行结束");
+		}
+
+	}
+}
